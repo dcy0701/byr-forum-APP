72a55b5eb6dc1ecf46d859d7f889081cf98725bf
dcy0701 <979325848@qq.com>
2016-12-03 20:05:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ function getValue(url, name) {
 
 nightmare
   .goto('http://bbs.byr.cn/oauth2/authorize?response_type=code&client_id=b7ad2efb085ee0ba87acd8ef921a0632&%20redirect_uri=http://www.byr.pub&%20state=abc')
-  .type('#Username', 'dcy0701')
-  .type('#Password', 'dcy0701')
+  .type('#Username', '')
+  .type('#Password', '')
   .click('#login_button')
   .forward()
   .wait(500)
